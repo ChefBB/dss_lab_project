dd790bd3075cde2c6ad2a27059cb04685761922c
Bazinga3 <b.barbieri7@studenti.unipi.it>
2025-11-26 11:15:02 +0100

    scoring function tracks firther update and debugging

--- assignment_2/integration.py
+++ assignment_2/integration.py
@@ -8,7 +8,7 @@ the original dataset.
 from difflib import SequenceMatcher
 
 
-def best_match_tracks(data_og: dict, data_retrieved: dict) -> dict:
+def compute_match_scores(data_og: dict, data_retrieved: dict) -> dict:
     """
     Computes matching score between original and retrieved items.
     
@@ -30,8 +30,8 @@ def best_match_tracks(data_og: dict, data_retrieved: dict) -> dict:
         flag = False
     for item in data_retrieved['recording-list' if flag else 'artist_list']:
         item['matching-score'] = (
-            matching_score_artist(data_og, item) if flag
-            else matching_score_track(data_og, item)
+            matching_score_track(data_og, item) if flag
+            else matching_score_artist(data_og, item)
         )
     
     return data_retrieved
@@ -64,10 +64,16 @@ def matching_score_track(track_og: dict, track_retrieved: dict) -> float:
         idx = len(track_retrieved.get('artist-credit')) + 1
         if ' feat. ' in track_retrieved.get('artist-credit'):
             idx = track_retrieved.get('artist-credit').index(' feat. ')
+        elif ' Feat ' in track_retrieved.get('artist-credit'):
+            idx = track_retrieved.get('artist-credit').index(' Feat ')
         primary_artists = track_retrieved.get('artist-credit')[:idx]
-        primary_artists = [artist for artist in primary_artists if artist != ' & ']
+        primary_artists = [artist for artist in primary_artists
+                           if isinstance(artist, dict)]
+        print(primary_artists)
         match *= max([
-            SequenceMatcher(None, track_og.get('primary_artist').lower(), artist['name'].lower())
+            SequenceMatcher(None,
+                            track_og.get('primary_artist').lower(),
+                            artist['name'].lower()).ratio()
             for artist in primary_artists
         ])
     
@@ -75,12 +81,15 @@ def matching_score_track(track_og: dict, track_retrieved: dict) -> float:
     if track_og.get('featured_artists') and ' feat. ' in track_retrieved.get('artist-credit'):
         idx = track_retrieved.get('artist-credit').index(' feat. ')
         featured_artists_retrieved = track_retrieved.get('artist-credit')[idx + 1:]
-        featured_artists_retrieved = [artist for artist in featured_artists_retrieved if artist != ' & ']
+        featured_artists_retrieved = [artist for artist in featured_artists_retrieved
+                                      if isinstance(artist, dict)]
         featured_artists_og = track_og.get('featured_artists').split(', ')
         # TODO: at this point, only takes the best matching og and retrieved featured artist
         # idea: take into account number of featured artists 
         match *= max([
-            SequenceMatcher(None, artist_og.lower(), artist_retrieved['name'].lower())
+            SequenceMatcher(None,
+                            artist_og.lower(),
+                            artist_retrieved['name'].lower()).ratio()
             for artist_og in featured_artists_og
             for artist_retrieved in featured_artists_retrieved
         ])
@@ -88,10 +97,13 @@ def matching_score_track(track_og: dict, track_retrieved: dict) -> float:
     # album
     # TODO check 'album'
     # release-list
-    if track_og.get('album_name'):
+    if track_og.get('album_name') and track_retrieved.get('release-list'):
+        # print(track_retrieved)
         album_name_og = track_og.get('album_name')
         match *= max(
-            [SequenceMatcher(None, album_name_og.lower(), release['title'].lower())
+            [SequenceMatcher(None,
+                             album_name_og.lower(),
+                             release['title'].lower()).ratio()
              for release in track_retrieved['release-list']]
         )
     
